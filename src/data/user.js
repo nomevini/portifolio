8158c41257bf93739bff0d3f8e36d2c01251bb64
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2023-09-11 10:42:33 -0300

    feat:beta-portifolio

---
+++ src/data/user.js
@@ -0,0 +1,95 @@
+
+
+const INFO = {
+	main: {
+		title: "nomevini",
+		name: "Vinícius de Sousa",
+		email: "sousav387@gmail.com",
+		logo: "../logo.jpg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/nome_vini",
+		github: "https://github.com/nomevini",
+		linkedin: "https://www.linkedin.com/in/nomevini/",
+		instagram: "https://instagram.com/nomevini",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/nomevini",
+	},
+
+	homepage: {
+		title: "Desenvolvedor Backend e Pesquisador Especializado em Visão Computacional.",
+		description:
+			"Sou desenvolvedor backend com experiência em Node.js. Tenho experiência na construção de aplicações web escaláveis, seguras e confiáveis ​​usando vários frameworks e tecnologias. Também realizo pesquisas na área de Visão Computacional e aprendizado de máquina. Gosto de resolver problemas complexos e aprender novas habilidades. Sou apaixonado por criar código de alta qualidade que segue as melhores práticas e padrões do setor. Estou sempre em busca de novos desafios e oportunidades para crescer como desenvolvedor.",
+	},
+
+	about: {
+		title: "I’m Tharindu N. I live in Sri Lanka, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+
+	logoLanguages: {
+		javascript: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		python: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+		postgre: "https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg",
+		tailwindCSS: "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+		html: "https://cdn.jsdelivr.net/npm/programming-languages-logos@0.0.3/src/html/html.png",
+	}
+	
+};
+
+export default INFO;
